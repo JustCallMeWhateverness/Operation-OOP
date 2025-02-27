b82d40a582261a435277f02fa01ce83ebf8fc4e3
Whateverness <nicolefritz00@gmail.com>
2025-02-27 10:49:56 +0100

    Added class files

---
+++ OperationOOP.Core/Models/Character.cs
@@ -0,0 +1,12 @@
+namespace OperationOOP.Core.Models;
+
+public class Character
+{
+    public int id {get; set;}
+    
+    public int Name { get; set;}
+  public int Level { get; set; }
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+}
